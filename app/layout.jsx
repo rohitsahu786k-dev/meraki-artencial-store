bec2feaa22c73a32a2975e4631b4a59056e150c0
rohitsahu786k-dev <rohitsahu786k@gmail.com>
2026-08-09 20:39:28 +0530

    Mount global cart drawer

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
 import { MobileBottomNav } from "@/components/mobile-bottom-nav";
+import { GlobalCartDrawer } from "@/components/global-cart-drawer";
 
 const montserrat = Montserrat({
   variable: "--font-retail",
@@ -33,6 +34,7 @@ export default function RootLayout({ children }) {
         <main>{children}</main>
         <Footer />
         <MobileBottomNav />
+        <GlobalCartDrawer />
       </body>
     </html>
   );
